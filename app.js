6701494a6acb508c568bb6632fa90e7d9874e83e
JOTHIR2004 <tkjoadi2004@gmail.com>
2024-06-28 19:19:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ hbs.registerPartials(partialsPath);
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
